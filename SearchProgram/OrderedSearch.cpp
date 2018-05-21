c161e340fb3a7e09d7a751958ef611ff90922c25
seenunit <seenunit@gmail.com>
2018-05-21 18:06:43 +0530

    Implemented Ordered search function

--- SearchProgram/OrderedSearch.cpp
+++ SearchProgram/OrderedSearch.cpp
@@ -13,7 +13,39 @@ OrderedSearch::~OrderedSearch()
 
 void OrderedSearch::SearchSequence(MatrixDataType matrix, vector<int> vecSequnce)
 {
-    cout << "Ordered Search of Sequence" << endl;
+    cout << "Ordered Search of Sequence rows: ";
 
-    return; //TODO
+	vector<int> vecOutPut;
+
+	for (int index = 0; index < matrix.size(); index++) {
+
+		auto row = matrix[index];
+
+		for (int i = 0; i < row.size(); i++) {
+
+			if (row[i] == vecSequnce[0]) {
+
+				// get sub row vector
+				vector<int> subRow(row.begin() + i, row.end());
+
+				for (int j = 0; j < vecSequnce.size() && j < subRow.size(); j++) {
+
+					if (vecSequnce[j] != subRow[j])
+						break;
+
+					if (j == vecSequnce.size() - 1)
+						vecOutPut.push_back(index + 1);
+				}
+			}
+		}
+	}
+
+	for (auto val : vecOutPut) {
+		cout << val << " ";
+	}
+
+	cout << endl;
+
+
+    return;
 }
