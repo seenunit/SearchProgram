3685523057ff99b9685479455f1496286fcf003a
Srinivas Reddy Challa <srinivasreddy.challa@outlook.com>
2018-05-25 14:08:15 +0530

    Rectified unordered search function

--- src/MatrixSearch.h
+++ src/MatrixSearch.h
@@ -8,8 +8,14 @@ using namespace std;
 
 typedef vector<vector<int>> MatrixDataType;
 
+// returns number of times sequence found in row
 int LinearSearchSequenceCount(vector<int> row, vector<int> sequence, bool bCount = false);
+
+// returns number of times value found in row
 int LinearSearchValueCount(vector<int> row, int x, bool bCount = false);
+
+// return row index of the value at first encounter
+int LinearSearchValue(vector<int> row, int x);
 
 class MatrixSearch
 {
--- src/UnOrderedSearch.cpp
+++ src/UnOrderedSearch.cpp
@@ -16,72 +16,64 @@ UnOrderedSearch::~UnOrderedSearch()
 
 void UnOrderedSearch::SearchSequence(MatrixDataType matrix, vector<int> vecSequnce)
 {
-    try {
+	try {
 
-        map<int, int> mapMatchCount;
+        vector<int> rowMatch;
+
+        vector<int> sortedSequence = vecSequnce;
+
+        // sort the sequence
+        sort(sortedSequence.begin(), sortedSequence.end());
 
         // search the sequence elements in each row
-        for (int i = 0; i < matrix.size(); i++) {
+		for (int i = 0; i < matrix.size(); i++) {
 
-            auto row = matrix[i];
+			auto row = matrix[i];
 
-            // sort the row for large row size
-            // TODD
+			// sort the row 
+            sort(row.begin(), row.end());
 
-            vector<int> vecValCount;
-            int prev = 0;
-            for (auto val : vecSequnce) {
+            bool hope = true;
+			for (auto val : sortedSequence) {
 
-                // use binary/interpolation search algorithm for optimization
-                // TODO
+				// use binary/interpolation search algorithm for optimization
+				// TODO
 
-                int count = LinearSearchValueCount(row, val, true);
+				int index = LinearSearchValue(row, val);
 
-                if (count == 0)
+                if (index == 0) {
+                    hope = false;
                     break;
-
-                if (prev == count)
-                    vecValCount.push_back(count);
-                else {
-                    if (prev == 0) {
-                        prev = count;
-                        vecValCount.push_back(count);
-                    }
-                    else
-                    {
-                        break;
-                    }
                 }
+
+                // remove the element at index or negate the value
+                row[index] = -row[index];
+			}
+
+            // if hope still there consider that row
+            if (hope == true) {
+                rowMatch.push_back(i + 1);
             }
-
-            if (vecValCount.size() == vecSequnce.size()) {
-                mapMatchCount.insert(make_pair(i + 1, vecValCount[0]));
-            }
-
+		}
+		
+		cout << "UnOrdered match rows: ";
+		// go thorugh match vector 
+		// display index of vector whose count more than 0
+        for (auto index : rowMatch) {
+            cout << index << " ";
         }
-
-        cout << "UnOrdered match rows: ";
-        // go thorugh match count vector 
-        // display index of vector whose count more than 0
-
-        for (auto it : mapMatchCount) {
-            cout << it.first << " ";
-        }
-
-        cout << endl;
-    }
-    catch (exception ex) {
-        cout << "Error: UnOrdered match of sequence failed due to: " << ex.what() << endl;
-    }
-
+		cout << endl;
+	}
+	catch (exception ex) {
+		cout << "Error: UnOrdered match of sequence failed due to: " << ex.what() << endl;
+	}
+	
 
     return; //TODO
-
 }
 
 // returns number of times value found in row
-int LinearSearchValueCount(vector<int> row, int x, bool bCount)
-{
+int LinearSearchValueCount(vector<int> row, int x, bool bCount) {
 	int count = 0;
 	for (auto val : row) {
 		if (val == x)
@@ -95,4 +87,16 @@ int LinearSearchValueCount(vector<int> row, int x, bool bCount)
 	}
 
 	return count;
+}
+
+// return row index of the value at first encounter
+int LinearSearchValue(vector<int> row, int x) {
+
+    for (size_t i = 0; i < row.size(); i++) {
+        if (row[i] == x)
+            return i + 1;
+    }
+
+    // return 0 if value not find
+    return 0;
 }
